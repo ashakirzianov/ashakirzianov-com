47370c74dcf2c6a572ccd2b9c2ab9dfd3196a23c
Anton Shakirzianov <ashakirzianov@icloud.com>
2023-03-27 16:41:03 -0700

    Default canvas size (2400x3200)

--- hooks/sketcher.ts
+++ hooks/sketcher.ts
@@ -3,10 +3,10 @@ import { useEffect } from "react";
 import { getCanvasFromRef, useCanvases } from "./canvas";
 
 export function useSketcher<State>(props: LaunchProps<State>) {
-    let { node, refs } = useCanvases(props.scene.dimensions ?? {
-        width: '100%',
-        height: '100%',
-    }, props.scene.layers.length);
+    let { node, refs } = useCanvases(Array(props.scene.layers.length).fill(props.scene.dimensions ?? {
+        width: 2400,
+        height: 3200,
+    }));
     useEffect(() => {
         let { launch } = launcher(props);
         let { cleanup } = launch(idx => getCanvasFromRef(refs[idx]));

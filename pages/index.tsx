3dd7202d0f37522ae0bf3ee9de2824997adb39dc
Anton Shakirzianov <ashakirzianov@icloud.com>
2023-04-02 22:19:04 -0700

    Refactor 'Sketch' component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,37 +3,9 @@ import { useSketcher } from "@/hooks/sketcher";
 import { Scene } from "@/sketcher";
 import { bwway, loveMeTwoTimes } from "@/sketches/posters";
 import { useRouter } from "next/router";
+import { ReactNode } from "react";
 
 // @refresh reset
-
-function Sketch({ sketch, offset, link }: {
-  sketch: Scene,
-  link: string,
-  offset?: Position,
-}) {
-  let { node } = useSketcher({
-    scene: sketch, period: 40,
-  });
-  let router = useRouter();
-  return <>
-    <Draggable
-      position={offset ?? { top: 0, left: 0 }}
-      onClick={() => router.push(link)}
-      cursor="pointer"
-    >
-      <div className="sketch">{node}</div>
-      <style jsx>{`
-    .sketch {
-      aspect-ratio: 3/4;
-      width: 200pt;
-      max-width: 50vw;
-      box-shadow: 0px 4px 16px 0px rgba(0,0,0,0.2);
-      border-radius: 5px;
-    }
-    `}</style>
-    </Draggable>
-  </>;
-}
 
 export default function Main() {
   return <div className="container">
@@ -70,4 +42,40 @@ export default function Main() {
       }
       `}</style>
   </div>;
+}
+
+function Post({ children, offset, link }: {
+  children: ReactNode,
+  link: string,
+  offset?: Position,
+}) {
+  let router = useRouter();
+  return <>
+    <Draggable
+      position={offset ?? { top: 0, left: 0 }}
+      onClick={() => router.push(link)}
+      cursor="pointer"
+    >
+      <div className="sketch">{children}</div>
+      <style jsx>{`
+    .sketch {
+      aspect-ratio: 3/4;
+      width: 200pt;
+      max-width: 50vw;
+      box-shadow: 0px 4px 16px 0px rgba(0,0,0,0.2);
+      border-radius: 5px;
+    }
+    `}</style>
+    </Draggable>
+  </>;
+}
+
+function Sketch({ sketch, link }: {
+  sketch: Scene,
+  link: string,
+}) {
+  let { node } = useSketcher({
+    scene: sketch, period: 40,
+  });
+  return <Post link={link}>{node}</Post>;
 }

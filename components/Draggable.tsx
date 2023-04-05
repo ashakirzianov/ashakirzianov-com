5671ba30e5cf00ffd7ba45b9bbc201a7c8486fc0
Anton Shakirzianov <ashakirzianov@icloud.com>
2023-04-04 20:19:05 -0700

    Draggable refactoring

--- components/Draggable.tsx
+++ components/Draggable.tsx
@@ -1,15 +1,13 @@
 import {
-    CSSProperties, ReactNode, TouchEvent, useEffect, useRef, useState,
+    CSSProperties, MouseEvent, ReactNode, useEffect, useRef, useState,
 } from "react";
 
 let globalZ = 0;
-export type Position = { top: number, left: number };
+export type Position = { x: number, y: number };
 export function Draggable({
-    position, children, onDrag, onStop, cursor,
+    children, onDrag, onStop,
 }: {
     children?: ReactNode,
-    position?: Position,
-    cursor?: CSSProperties['cursor'],
     onDrag?: () => void,
     onStop?: () => void,
 }) {
@@ -22,10 +20,31 @@ export function Draggable({
     let [zIndex, setZIndex] = useState(globalZ);
     let [cursorChanged, setCursorChanged] = useState(false);
 
-    function handleStartDragging() {
+    function handleStartDragging({ x, y }: Position) {
         setDragging(true);
         setZIndex(() => globalZ++);
         setCursorChanged(true);
+        setState(state => ({
+            ...state,
+            touchStart: {
+                x: state.offset.x - x,
+                y: state.offset.y - y,
+            }
+        }));
+    }
+    function handleDragging({ x, y }: Position) {
+        if (dragging) {
+            setState(state => ({
+                ...state,
+                offset: {
+                    x: state.touchStart.x + x,
+                    y: state.touchStart.y + y,
+                },
+            }));
+            if (onDrag) {
+                onDrag();
+            }
+        }
     }
     function handleEndDragging() {
         if (onStop) {
@@ -35,48 +54,36 @@ export function Draggable({
         setCursorChanged(false);
     }
 
-    useEffect(() => {
-        function getPosition(touch: Touch) {
-            return {
-                x: touch.clientX,
-                y: touch.clientY,
-            };
+    function getTouchPosition(event: globalThis.TouchEvent) {
+        let touches = event.targetTouches;
+        if (touches.length !== 1) {
+            return undefined;
         }
+        let touch = event.targetTouches[0]!;
+        return {
+            x: touch.clientX,
+            y: touch.clientY,
+        };
+    }
+
+    function getMousePosition({ clientX, clientY }: MouseEvent<unknown>) {
+        return {
+            x: clientX, y: clientY,
+        };
+    }
+
+    useEffect(() => {
         function handleTouchStart(event: globalThis.TouchEvent) {
-            let touches = event.targetTouches;
-            if (touches.length !== 1) {
-                return;
+            let position = getTouchPosition(event);
+            if (position) {
+                handleStartDragging(position);
             }
-            let touch = event.targetTouches[0]!;
-            handleStartDragging();
-            let { x, y } = getPosition(touch);
-            setState(state => ({
-                ...state,
-                touchStart: {
-                    x: state.offset.x - x,
-                    y: state.offset.y - y,
-                }
-            }));
         }
         function handleTouchMove(event: globalThis.TouchEvent) {
-            let touches = event.targetTouches;
-            if (touches.length !== 1) {
-                return;
-            }
-            let touch = event.targetTouches[0]!;
-            event.preventDefault();
-            if (dragging) {
-                let { x, y } = getPosition(touch);
-                setState(state => ({
-                    ...state,
-                    offset: {
-                        x: state.touchStart.x + x,
-                        y: state.touchStart.y + y,
-                    },
-                }));
-                if (onDrag) {
-                    onDrag();
-                }
+            let position = getTouchPosition(event);
+            if (position) {
+                event.preventDefault();
+                handleDragging(position);
             }
         }
         function handleTouchEnd() {
@@ -113,25 +120,16 @@ export function Draggable({
 
     return <div ref={divRef} style={{
         position: 'relative',
-        left: (position?.left ?? 0) + state.offset.x,
-        top: (position?.top ?? 0) + state.offset.y,
+        left: state.offset.x,
+        top: state.offset.y,
         zIndex,
         cursor: cursorChanged ? 'grab' : undefined,
     }}
-        onMouseDown={handleStartDragging}
-        onMouseMove={function ({ movementX, movementY }) {
-            if (dragging) {
-                setState(state => ({
-                    ...state,
-                    offset: {
-                        x: state.offset.x + movementX,
-                        y: state.offset.y + movementY,
-                    },
-                }));
-                if (onDrag) {
-                    onDrag();
-                }
-            }
+        onMouseDown={function (event) {
+            handleStartDragging(getMousePosition(event));
+        }}
+        onMouseMove={function (event) {
+            handleDragging(getMousePosition(event));
         }}
         onMouseEnter={function () {
             setTimeout(() => setCursorChanged(true), 1000);

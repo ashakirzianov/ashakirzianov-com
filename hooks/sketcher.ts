e54e6f0e9b147df232148a941d44bf4a4f81bcd4
Anton Shakirzianov <ashakirzianov@icloud.com>
2023-04-07 20:38:57 -0700

    Move 'dimensions' option to 'useSketcher' hook

--- hooks/sketcher.ts
+++ hooks/sketcher.ts
@@ -2,7 +2,9 @@ import { LaunchProps, launcher } from "@/sketcher";
 import { useEffect } from "react";
 import { getCanvasFromRef, useCanvases } from "./canvas";
 
-export function useSketcher<State>(props: LaunchProps<State>) {
+export function useSketcher<State>(props: LaunchProps<State> & {
+    dimensions?: [width: number, height: number],
+}) {
     let dims = Array(props.scene.layers.length).fill(props.dimensions ?? props.scene.dimensions ?? [undefined, undefined]);
     let { node, refs } = useCanvases(dims);
     useEffect(() => {

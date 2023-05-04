dfd0c2ee56d0421c7792fd374a8ac6c7439d21be
Anton Shakirzianov <ashakirzianov@icloud.com>
2023-05-04 13:54:08 -0700

    Different way of using GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Press_Start_2P } from '@next/font/google'
 import { Cormorant } from '@next/font/google'
-import useGoogleAnalytics from '@/utils/googleAnalytics'
+import Script from 'next/script'
 
 const textFont = Cormorant({
   subsets: ['cyrillic-ext'],
@@ -17,8 +17,20 @@ const p2p = Press_Start_2P({
 })
 
 export default function App({ Component, pageProps }: AppProps) {
-  useGoogleAnalytics('G-SE6H2FX2N9')
   return <main className={`${textFont.variable} ${p2p.variable}`}>
+    <Script
+      src="https://www.googletagmanager.com/gtag/js?id=G-SE6H2FX2N9"
+      strategy="afterInteractive"
+    />
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-SE6H2FX2N9');
+        `}
+    </Script>
     <Component {...pageProps} />
   </main>
 }

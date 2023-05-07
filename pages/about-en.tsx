1ccdfa4796125551c6ca00e27004134aa3e0b538
Anton Shakirzianov <ashakirzianov@icloud.com>
2023-05-06 21:38:10 -0700

    Minor about-en change

--- pages/about-en.tsx
+++ pages/about-en.tsx
@@ -17,7 +17,7 @@ export default function AboutPage() {
         <AboutCard>
             <h1>{`Help me, I don't speak the language!`}</h1>
             <p>{`Hey, this is just my personal page. My name is Anton Shakirzianov, but I use a pen name "Andjan" (or "Анҗан" in Cyrillics).`}</p>
-            <p>I do <AboutLink href={href('sketch', { hue })}>generative art</AboutLink>. I also write <AboutLink href={href('text', { hue })}>short stories</AboutLink> in Russian. They are fiction, fact-meets-fiction, and everything in-between.</p>
+            <p>I do <AboutLink href={href('sketch', { hue })}>generative art</AboutLink>. I also write <AboutLink href={href('text', { hue })}>short stories</AboutLink> in my native language. They are fiction, fact-meets-fiction, and everything in-between.</p>
             <p>I have <AboutLink href='https://instagram.com/ashakirzianov'>instagram</AboutLink> and <AboutLink href='https://t.me/ashakirzianov_live'>telegram</AboutLink>.</p>
         </AboutCard>
     </PixelPage>

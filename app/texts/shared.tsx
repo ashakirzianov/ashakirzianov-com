1945d403ddacfb012138a7e6656365f0c4e959e3
Anton Shakirzianov <ashakirzianov@icloud.com>
2023-05-25 01:07:27 -0700

    Fix build

--- app/texts/shared.tsx
+++ app/texts/shared.tsx
@@ -9,13 +9,9 @@ export function AllStoritesPage({ previews, language }: {
     previews: TextPostMap,
     language?: Language,
 }) {
-    let title = language === 'en' ? 'All stories' : 'Все рассказы'
-    let description = language === 'en' ? 'All stories' : 'Страница со всеми рассказами'
     let pairs = Object.entries(previews)
         .sort(([, a], [, b]) => b?.date?.localeCompare(a?.date ?? '') ?? 0)
     return <PixelPage
-        title={title}
-        description={description}
     >
         <div className="flex flex-col items-center justify-start max-w-collection">
             <div className="flex flex-row flex-wrap content-start gap-stn p-stn">

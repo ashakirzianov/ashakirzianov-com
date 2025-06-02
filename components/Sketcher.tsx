e77bea614fa5ae1091bd20688c84f9dee9423b9f
Anton Shakirzianov <ashakirzianov@icloud.com>
2025-06-02 17:37:58 -0300

    Comform to hook rules

--- components/Sketcher.tsx
+++ components/Sketcher.tsx
@@ -4,31 +4,35 @@ import { useEffect } from 'react'
 import { getCanvasFromRef, useCanvases } from '@/utils/canvas'
 
 type SketcherProps<S> = Omit<LaunchProps<S>, 'getCanvas'>
-export function Sketcher<State>(props: SketcherProps<State> & {
+export function Sketcher<State>({
+    scene, dimensions, period, skip, chunk
+}: SketcherProps<State> & {
     dimensions?: [width: number, height: number],
 }) {
-    const dims = Array(props.scene.layers.length).fill(props.dimensions ?? props.scene.dimensions ?? [undefined, undefined])
-    const { node, refs } = useCanvases(dims)
+    const actualDimensions = dimensions ?? scene.dimensions ?? [undefined, undefined]
+    const { node, refs } = useCanvases(actualDimensions, scene.layers.length)
     useEffect(() => {
         const { start, cleanup } = launcher({
-            ...props,
+            scene,
+            period, skip, chunk,
             getCanvas: idx => getCanvasFromRef(
                 refs[idx],
-                props.scene.layers[idx]?.kind === '3d' ? 'webgl' : '2d',
+                scene.layers[idx]?.kind === '3d' ? 'webgl' : '2d',
             ),
         })
         start()
 
         return cleanup
-    }, [])
+    }, [node, refs, scene, period, skip, chunk])
     return node
 }
 
 export function useSketcherPlayer<State>(props: SketcherProps<State> & {
     dimensions?: [width: number, height: number],
 }) {
-    const dims = Array(props.scene.layers.length).fill(props.dimensions ?? props.scene.dimensions ?? [undefined, undefined])
-    const { node, refs } = useCanvases(dims)
+    const dimensions = props.dimensions ?? props.scene.dimensions ?? [undefined, undefined]
+    const layers = props.scene.layers
+    const { node, refs } = useCanvases(dimensions, layers.length)
     const { start, pause, isPaused, cleanup } = launcher({
         ...props,
         getCanvas: idx => getCanvasFromRef(

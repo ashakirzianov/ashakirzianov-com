f0c681af9ecf43d2d44c735e95c04eb556310f9d
Anton Shakirzianov <ashakirzianov@icloud.com>
2025-07-13 15:07:02 -0300

    3d playground

---
+++ sketches/3d.ts
@@ -0,0 +1,35 @@
+import { Scene } from '@/sketcher'
+import { WebGLRenderer, Scene as ThreeScene, PerspectiveCamera, BoxGeometry, MeshPhongMaterial, Mesh, DirectionalLight } from 'three'
+
+export function playground3dScene(): Scene<any> {
+    return {
+        id: 'playground',
+        state: {},
+        layers: [{
+            kind: '3d',
+            render({ canvas: { context, width, height }, frame }) {
+                const r = new WebGLRenderer({
+                    context,
+                    canvas: context.canvas,
+                })
+                const scene = new ThreeScene()
+                const camera = new PerspectiveCamera(75, width / height, 0.1, 1000)
+                const geometry = new BoxGeometry(1, 1, 1)
+                const material = new MeshPhongMaterial({ color: 0x00ff00 })
+                const cube = new Mesh(geometry, material)
+                scene.add(cube)
+
+                const intensity = 1
+                const light = new DirectionalLight('#aaaaaa', intensity)
+                light.position.set(-1, 2, 4)
+                scene.add(light)
+
+                camera.position.z = 5
+                camera.position.y = Math.sin(frame / 10) * 2
+                camera.position.x = Math.cos(frame / 10) * 2
+                camera.lookAt(0, 0, 0)
+                r.render(scene, camera)
+            },
+        }],
+    }
+}
